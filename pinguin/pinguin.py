9afecf5422d163e537eccbe934ed633b95012349
dimayosky <dimayosky@gmail.com>
2021-11-11 23:35:35 +0300

    refactor

--- pinguin/pinguin.py
+++ pinguin/pinguin.py
@@ -1,12 +1,11 @@
 HowManyPenguins = input()
 IsNumber = HowManyPenguins.isnumeric()
 if IsNumber:
-        int(HowManyPenguins)
-#HowManyPenguins = int(input())
-pinguinRow1 =        "   _~_    " * int(HowManyPenguins)
-pinguinRow2 = "\n" + "  (o o)   " * int(HowManyPenguins)
-pinguinRow3 = "\n" + " /  V  \  " * int(HowManyPenguins)
-pinguinRow4 = "\n" + "/(  _  )\ " * int(HowManyPenguins)
-pinguinRow5 = "\n" + "  ^^ ^^   " * int(HowManyPenguins)
-pinguin = pinguinRow1 + pinguinRow2 + pinguinRow3+ pinguinRow4+ pinguinRow5
+    int(HowManyPenguins)
+# HowManyPenguins = int(input())
+pinguin =        ("   _~_    " * int(HowManyPenguins) +
+           "\n" + "  (o o)   " * int(HowManyPenguins) +
+           "\n" + " /  V  \  " * int(HowManyPenguins) +
+           "\n" + "/(  _  )\ " * int(HowManyPenguins) +
+           "\n" + "  ^^ ^^   " * int(HowManyPenguins))
 print(pinguin)
